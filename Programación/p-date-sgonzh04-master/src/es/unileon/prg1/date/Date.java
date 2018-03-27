b76476972b4430380d21474c650a9201533acff5
Your Name <you@example.com>
2018-03-27 12:37:19 +0200

    second commit

---
+++ Programación/p-date-sgonzh04-master/src/es/unileon/prg1/date/Date.java
@@ -0,0 +1,57 @@
+package es.unileon.prg1.date;
+
+public class Date {
+	private int day;
+	private int month;
+	private int year;
+	
+	// Constructor mal programado: Permite crear fechas no validas
+	public Date(int day, int month, int year){
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public int getYear(){
+		return this.year;
+	}
+
+	public int getMonth(){
+		return this.month;
+	
+	}
+	
+	public int getDay(){
+		return this.day;
+	}
+
+
+	boolean isSameYear(Date another){
+		if ( this.year == another.getYear() ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	boolean isSameMonth(Date another){
+		if(this.month == another.getMonth()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	boolean isSameDay(Date another){
+		if(this.day == another.getDat()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public String toString(){
+		return this.day + "/" + this.month + "/" + this.year;
+	}
+
+}
